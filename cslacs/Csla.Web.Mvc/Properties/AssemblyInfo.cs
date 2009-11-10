78a809caf769f3146d0261bc31cf27fb2626b1f3
rocky@lhotka.net <rocky@lhotka.net@fa5919ed-4f44-5740-a13a-40f41935fe31>
2009-11-10 18:32:54 +0000

    Remove MVC requirement from 3.8.x bugid: 640

--- cslacs/Csla.Web.Mvc/Properties/AssemblyInfo.cs
+++ cslacs/Csla.Web.Mvc/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("cslalighttest web")]
-[assembly: AssemblyDescription("CSLA .NET for Silverlight test web")]
+[assembly: AssemblyTitle("CSLA .NET for Windows")]
+[assembly: AssemblyDescription("CSLA .NET ASP.NET MVC Library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Marimer LLC")]
 [assembly: AssemblyProduct("CSLA .NET")]
-[assembly: AssemblyCopyright("Copyright ©  2009 Marimer LLC")]
+[assembly: AssemblyCopyright("Copyright © 2009 Marimer LLC")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3d5900ae-111a-45be-96b3-d9e4606ca793")]
+[assembly: Guid("bddeb05a-95f7-4711-9b99-cba6fed61ee0")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.8.1.0")]
 [assembly: AssemblyFileVersion("3.8.1.0")]
